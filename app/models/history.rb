7a79438f64eb49579a5f4e45b2f166bfedc0157e
Ev1lxg3n1us <evilx.emelyanov@face-team.com>
2019-03-01 17:01:49 +0200

    change carousel slide change speed && fix bug with history

--- app/models/history.rb
+++ app/models/history.rb
@@ -3,7 +3,7 @@
 class History
   include Mongoid::Document
 
-  field :taken_in,    type: Time,   default: Time.now
+  field :taken_in,    type: Time,   default: ->{ Time.now }
   field :returned_in, type: Time
 
   belongs_to  :user
